008a77e937d201c0469e1090239ea42977bc9afc
Ryan <windask@hotmail.com>
2018-06-11 03:02:34 -0700

    Add message for unauthorized

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import StarsIcon from '@material-ui/icons/Stars';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
 
 import { auth, database } from './../client';
 import MirumTable from './MirumTable.jsx';
 import QuestionsTable from './QuestionsTable.jsx';
 import EditDialog from './EditDialog.jsx';
-
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import StarsIcon from '@material-ui/icons/Stars';
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 
 function renderUnauthorizedContent() {
   return <span>You must be logged in/authorized to see this.</span>;
@@ -45,7 +44,7 @@ class Main extends React.Component {
             if (tableEntries) {
               this.setState({ tableEntries });
             }
-          });
+          }, () => this.setState({ tableEntries: null }));
 
           database.ref('question').on('value', (snapshot) => {
             const questions = snapshot.val();
@@ -162,35 +161,45 @@ class Main extends React.Component {
 
   renderPoints() {
     return (
-      <span>
-        {this.renderAnalytics()}
-        <div className="row">
-          <div className="col-md-12">
-            <Button
-              variant="contained"
-              className="pull-right"
-              onClick={this.handleOpen}
-              color="secondary"
-            >
-              <AddIcon />
+      this.state.tableEntries === null
+        ? 'You are probably not authorized for this feature. Contact mirum@ryanly.ca.'
+        :
+        (
+          <span>
+            {this.renderAnalytics()}
+            <div className="row">
+              <div className="col-md-12">
+                <Button
+                  variant="contained"
+                  className="pull-right"
+                  onClick={this.handleOpen}
+                  color="secondary"
+                >
+                  <AddIcon />
               Add points
-            </Button>
-            <EditDialog
-              tableEntry={this.state.tableEntry}
-              users={this.state.users}
-              open={this.state.open}
-              onDone={this.handleClose}
-              update={false}
-            />
-          </div>
-        </div>
-        <MirumTable users={this.state.users} tableEntries={this.state.tableEntries} />
-      </span>
+                </Button>
+                <EditDialog
+                  tableEntry={this.state.tableEntry}
+                  users={this.state.users}
+                  open={this.state.open}
+                  onDone={this.handleClose}
+                  update={false}
+                />
+              </div>
+            </div>
+            <MirumTable users={this.state.users} tableEntries={this.state.tableEntries} />
+          </span>
+        )
     );
   }
 
   renderQuestions() {
-    return <QuestionsTable currentUserID={this.state.user.uid} users={this.state.users} questions={this.state.questions} answers={this.state.answers} />;
+    return (<QuestionsTable
+      currentUserID={this.state.user.uid}
+      users={this.state.users}
+      questions={this.state.questions}
+      answers={this.state.answers}
+    />);
   }
 
   render() {
